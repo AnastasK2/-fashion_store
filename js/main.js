65a24f44804abb2bff183747b3040d648ba9af79
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-12-15 16:37:59 +0300

    create: information

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@ var menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', function () {
   document.querySelector(".header__block").classList.toggle("header__block--visible");
-
 });
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal (){
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+function closeModal (event){
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
