3bb0f3c76d6446453a8f0851a7a6d313e3d630b5
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2020-12-20 19:01:02 +0300

    переделала продкуты и добавила табы

--- js/main.js
+++ js/main.js
@@ -4,6 +4,29 @@ menuButton.addEventListener('click', function () {
   document.querySelector(".header__block").classList.toggle("header__block--visible");
 });
 
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
 var mySwiper = new Swiper('.reviews-slider', {
   // Optional parameters
   direction: 'horizontal',
